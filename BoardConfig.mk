6b2a8b6cbf5420fe82d8bbaa0ed83540f7b3a294
nguyễn hoàng vĩnh thịnh <nthinh1102@gmail.com>
2016-11-27 19:09:56 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,10 +17,6 @@ BOARD_VENDOR := xiaomi
 
 CANCRO_PATH := device/xiaomi/cancro
 
-# ReleaseTools
-TARGET_RELEASETOOLS_EXTENSIONS := $(CANCRO_PATH)/releasetools
-TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cancro
-
 TARGET_BOARD_INFO_FILE ?= $(CANCRO_PATH)/board-info.txt
 
 # Bootloader
