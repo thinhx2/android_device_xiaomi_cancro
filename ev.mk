51228110b67ca076ec21e9a85d79957ed0ebc5b4
nguyễn hoàng vĩnh thịnh <nthinh1102@gmail.com>
2016-11-30 19:42:45 +0700

    fix boot anim

--- ev.mk
+++ ev.mk
@@ -13,11 +13,10 @@
 # limitations under the License.
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+BOOT_ANIMATION_SIZE := 1080p
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/ev/config/common_full_phone.mk)
