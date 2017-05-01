2baf318330ccb46d9b5d883585818c7806b36046
Nguyễn Hoàng Vĩnh Thịnh <nthinh1102@gmail.com>
2017-05-01 16:48:14 +0700

    Angler: Compile libhwui for performance, Enable real time lockscreen charging current values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,12 @@ BOARD_USES_QC_TIME_SERVICES := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO 
+
+# Compile libhwui in performance mode
+HWUI_COMPILE_FOR_PERF := true
+ 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
   ifeq ($(TARGET_BUILD_VARIANT),user)
