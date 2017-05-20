12598b8fffe0f235e8908ddc297690adf2447694
Nguyễn Hoàng Vĩnh Thịnh <nthinh1102@gmail.com>
2017-05-20 14:43:30 +0700

    Update aosp_cancro.mk

--- aosp_cancro.mk
+++ aosp_cancro.mk
@@ -19,13 +19,13 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common HalogenOS stuff.
+$(call inherit-product, vendor/xos/config/common.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := aosp_cancro
+PRODUCT_NAME := XOS_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
