2361d5abebf0f131ef833f96a88240e3a2584d1f
thinhx2 <nthinh1102@gmail.com>
2017-07-17 20:36:37 +0700

    Update and rename viper.mk to cmremix.mk

--- cmremix.mk
+++ cmremix.mk
@@ -20,12 +20,12 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/viper/config/common_full_phone.mk)
+$(call inherit-product, vendor/cmremix/config/common_full_phone.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := viper_cancro
+PRODUCT_NAME := cmremix_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
