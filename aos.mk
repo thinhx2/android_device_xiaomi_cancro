406a01d90802e95bbafd1558a47811c9c30c4fec
thinhx2 <nthinh1102@gmail.com>
2017-07-23 11:36:24 +0700

    Update and rename aosp_cancro.mk to aos.mk

--- aos.mk
+++ aos.mk
@@ -20,12 +20,12 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/dark/common.mk)
+$(call inherit-product, vendor/aos/config/common_full_phone.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := aosp_cancro
+PRODUCT_NAME := aos_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
